3450a91b4e9c4a9536f68819242f480294a5434e
nnnnameGit <aaaaddress>
2021-10-12 10:30:50 +0900

    delete template-comment

--- FlipByBlink_ver3/ViewController.swift
+++ FlipByBlink_ver3/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  FlipByBlink_ver3
-//
-//  Created by nnname on 2021/10/12.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
