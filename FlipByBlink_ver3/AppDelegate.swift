3d42195935f661b38e521bf730b6e0efb204c24d
nnnnameGit <aaaaddress>
2021-10-19 19:09:58 +0900

    mini

--- FlipByBlink_ver3/AppDelegate.swift
+++ FlipByBlink_ver3/AppDelegate.swift
@@ -8,15 +8,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
-
+    /* "Xcode template source code"
     // MARK: UISceneSession Lifecycle
 
-//    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-//        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-//    }
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
 
-//    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-//    }
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+     */
 
 
 }
--- FlipByBlink_ver3/SceneDelegate.swift
+++ FlipByBlink_ver3/SceneDelegate.swift
@@ -5,26 +5,27 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    /* "Xcode template source code"
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
 
-//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-//        guard let _ = (scene as? UIWindowScene) else { return }
-//    }
-//
-//    func sceneDidDisconnect(_ scene: UIScene) {
-//    }
-//
-//    func sceneDidBecomeActive(_ scene: UIScene) {
-//    }
-//
-//    func sceneWillResignActive(_ scene: UIScene) {
-//    }
-//
-//    func sceneWillEnterForeground(_ scene: UIScene) {
-//    }
-//
-//    func sceneDidEnterBackground(_ scene: UIScene) {
-//    }
-//    
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+    */
+    
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
         
         if let 🎮 = window?.rootViewController as? ViewController{
