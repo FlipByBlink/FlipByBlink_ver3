d657ea02fed7888ac3b40f167bbfcc12260142c6
nnnnameGit <aaaaddress>
2021-10-19 19:04:53 +0900

    mini

--- FlipByBlink_ver3/AppDelegate.swift
+++ FlipByBlink_ver3/AppDelegate.swift
@@ -5,9 +5,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//        return true
-//    }
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
 
     // MARK: UISceneSession Lifecycle
 
